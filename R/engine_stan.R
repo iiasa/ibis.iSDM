d4f95e30ce2f14237c31b7b8047c3d3bc0283e69
Martin Jung <martinjung@zoho.com>
2021-08-16 17:08:43 +0200

    First attempt for stan files

--- R/engine_stan.R
+++ R/engine_stan.R
@@ -3,12 +3,25 @@ NULL
 #' Use STAN as engine
 #'
 #' @param x [distribution()] (i.e. [`BiodiversityDistribution-class`]) object.
+#' @param chains A positive integer specifying the number of Markov chains. The default is 4.
+#' @param iter A positive integer specifying the number of iterations for each chain (including warmup). The default is 2000.
+#' @param warmup positive integer specifying the number of warmup (aka burnin) iterations per chain. If step-size adaptation is on (which it is by default), this also controls the number of iterations for which adaptation is run (and hence these warmup samples should not be used for inference). The number of warmup iterations should be smaller than iter and the default is iter/2.
+#' @param cores If set to NULL take values from specified ibis option getOption('ibis.nthread')
+#' @param init Initial values for parameters. Default: 'random'. Can also be specified as list (see: rstan::stan)
+#' @param control See rstan::stan for more details on specifying the controls
 #' @param ... Other variables
+#' @seealso rstan
 #' @name engine_stan
 NULL
 #' @rdname engine_stan
 #' @export
 engine_stan <- function(x,
+                        chains = 4,
+                        iter = 2000,
+                        warmup = 500,
+                        init = "random",
+                        cores = NULL,
+                        control = NULL,
                         ...) {
   # Check whether INLA package is available
   check_package('rstan')
@@ -17,10 +30,21 @@ engine_stan <- function(x,
   assertthat::assert_that(inherits(x, "BiodiversityDistribution"),
                           inherits(x$background,'sf')
   )
-  # Use stan directly or rstanarm for now (latter impossible to specify spatial latent effects yet, but see  https://github.com/stan-dev/rstanarm/issues/207)
+  # Other checks of parameters
+  assertthat::assert_that(
+    is.numeric(chains), is.numeric(iter), is.numeric(warmup),
+    is.null(cores) || is.numeric(cores),
+    is.character(init) || is.list(init),
+    is.null(control) || is.list(control),
+    msg = 'Input parameters wrongly specified!'
+  )
+  # CHECK:
+  # Use stan directly or rstanarm for now (latter impossible to specify spatial latent effects yet, but see https://github.com/stan-dev/rstanarm/issues/207)
   # https://github.com/ConnorDonegan/Stan-IAR
-  # BRMS joint modelling?
   # rstanarm object to improve speed through approximation
+
+  if(is.null(cores)) cores <- getOption('ibis.nthread')
+
   # Set engine in distribution object
   x$set_engine(
     bdproto(
@@ -28,13 +52,35 @@ engine_stan <- function(x,
       Engine,
       name = "<STAN>",
       data = list(),
+      # Stan options
+      stan_param = list(
+        chains = chains, iter = iter,
+        warmup = warmup, init = init,
+        cores = cores, control = control,
+      ),
+      # Function to respecify the control parameters
+      set_control = function(self,
+                             chains = 4,
+                             iter = 2000,
+                             warmup = 500,
+                             init = "random",
+                             cores = NULL,
+                             control = NULL){
+
+        # Overwrite existing
+        self$stan_param <- list(
+          chains = chains, iter = iter,
+          warmup = warmup, init = init,
+          cores = cores, control = control,
+        )
+
+      },
       # Setup a model
       setup = function(self){
         # Parallel processing
-        options(mc.cores = parallel::detectCores())
+
       },
       train = function(self){
-
       }
     )
   ) # End of engine definition
---
+++ inst/stan/lm_stan.stan
@@ -0,0 +1,31 @@
+//
+// This Stan program defines a simple model, with a
+// vector of values 'y' modeled as normally distributed
+// with mean 'mu' and standard deviation 'sigma'.
+//
+// Learn more about model development with Stan at:
+//
+//    http://mc-stan.org/users/interfaces/rstan.html
+//    https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
+//
+
+// The input data is a vector 'y' of length 'N'.
+data {
+  int<lower=0> N;
+  vector[N] x;
+  vector[N] y;
+}
+
+// The parameters accepted by the model. Our model
+// accepts two parameters 'mu' and 'sigma'.
+parameters {
+  real intercept;
+  real beta;
+  real<lower=0> sigma;
+}
+
+// Linear model
+model {
+  y ~ normal(intercept + beta * x, sigma);
+}
+
